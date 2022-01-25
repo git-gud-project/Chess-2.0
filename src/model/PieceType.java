ac065635452edac5dc86052b03cefcf36fed453e
Wincent Holm <wincent.holm@gmail.com>
2022-01-25 16:38:50 +0100

    Added file prefix and suffix variables

--- src/model/PieceType.java
+++ src/model/PieceType.java
@@ -1,10 +1,20 @@
 package model;
 
 public enum PieceType {
-    BISHOP,
-    KING,
-    KNIGHT,
-    PAWN,
-    QUEEN,
-    ROOK
+    BISHOP("b"),
+    KING("k"),
+    KNIGHT("h"),
+    PAWN("p"),
+    QUEEN("q"),
+    ROOK("t");
+
+    private String _filePrefix;
+
+    private PieceType(String prefix) {
+        _filePrefix = prefix;
+    }
+    
+    public String getFilePrefix() {
+        return _filePrefix;
+    }
 }
