ebba632dfc15d3e5f6ad37c255b25eeb74b678fb
Wincent Holm <wincent.holm@gmail.com>
2022-01-25 15:15:09 +0100

    Some refactoring

--- src/view/ChessView.java
+++ src/view/ChessView.java
@@ -3,76 +3,32 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ChessView extends JFrame {
+
+    public final int DEFAULT_WINDOW_WIDTH = 600;
+    public final int DEFAULT_WINDOW_HEIGHT = 600;
+    public final boolean DEFAULT_RESIZABLE = false;
+    public final String DEFAULT_TITLE = "Chess Game";
+
+    private BoardPanel _boardPanel;
     
     public ChessView() {
-        super("Chess Game");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(new Dimension(600, 600));
+        this.setTitle(DEFAULT_TITLE);
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.setSize(new Dimension(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT));
+        
+        this.setResizable(DEFAULT_RESIZABLE);
 
         this.setLayout(new BorderLayout());
 
-        var pane = new JPanel();
-        
-        CellButton button;
+        // Create board panel
+        _boardPanel = new BoardPanel(8);
 
-        pane.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        
-        final int SIZE = 8;
+        this.add(_boardPanel, BorderLayout.CENTER);
 
-        for (int i = 0; i < SIZE; ++i) {
-            button = new CellButton(String.valueOf((char) (i + 'A')));
-            c.fill = GridBagConstraints.HORIZONTAL;
-            c.gridx = i + 1;
-            c.gridy = 0;
-            pane.add(button, c);
-            button.setMinimumSize(new Dimension(60, 20));
-            button.setPreferredSize(new Dimension(80, 40));
-        }
+        this.pack();
 
-        for (int i = 0; i < SIZE; ++i) {
-            button = new CellButton(Integer.toString(SIZE - i));
-            c.fill = GridBagConstraints.VERTICAL;
-            c.gridx = 0;
-            c.gridy = i + 1;
-            pane.add(button, c);
-            button.setMinimumSize(new Dimension(20, 60));
-            button.setPreferredSize(new Dimension(40, 80));
-        }
-        
-        for (int i = 0; i < SIZE; ++i) {
-            button = new CellButton(String.valueOf((char) (i + 'A')));
-            c.fill = GridBagConstraints.HORIZONTAL;
-            c.gridx = i + 1;
-            c.gridy = SIZE + 1;
-            pane.add(button, c);
-            button.setMinimumSize(new Dimension(60, 20));
-            button.setPreferredSize(new Dimension(80, 40));
-        }
-
-        for (int i = 0; i < SIZE; ++i) {
-            button = new CellButton(Integer.toString(SIZE - i));
-            c.fill = GridBagConstraints.VERTICAL;
-            c.gridx = SIZE + 1;
-            c.gridy = i + 1;
-            pane.add(button, c);
-            button.setMinimumSize(new Dimension(20, 60));
-            button.setPreferredSize(new Dimension(40, 80));
-        }
-
-        JPanel grid = new BoardPanel(SIZE);
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 0.0;
-        c.gridwidth = 8;
-        c.gridheight = 8;
-        c.gridx = 1;
-        c.gridy = 1;
-        pane.add(grid, c);
-
-        this.add(pane, BorderLayout.CENTER);
-
-        pack();
-
-        setVisible(true);
+        this.setVisible(true);
     }
 }
