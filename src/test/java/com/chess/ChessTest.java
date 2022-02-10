f62e2837927d0d7a696348fdd3775b23d9a763ca
Wincent Holm <wincent.holm@gmail.com>
2022-02-10 15:57:09 +0100

    Simple test file

---
+++ src/test/java/com/chess/ChessTest.java
@@ -0,0 +1,16 @@
+package com.chess;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ChessTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
