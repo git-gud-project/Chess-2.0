9ab3affcdd3a2687618f3229b7a104bc5208afc4
IsakHolm <holmdahl.isak@gmail.com>
2022-02-22 14:54:41 +0100

    Added BoardInformation interface and ChessBoardInformation class. Added ChessBoardInformation instance in ChessModel

---
+++ src/main/java/com/chess/model/BoardInformation.java
@@ -0,0 +1,13 @@
+package com.chess.model;
+
+public interface BoardInformation {
+    public boolean isEmpty(Position position);
+    public boolean isEmpty(int row, int col);
+
+    public Identifier getTeamIdentifier(Position position);
+
+    public Identifier getTypeIdentifier(Position position);
+
+    public int getBoardSize();
+    }
+}
---
+++ src/main/java/com/chess/model/ChessBoardInformation.java
@@ -0,0 +1,39 @@
+package com.chess.model;
+
+public class ChessBoardInformation implements BoardInformation {
+
+    private final Board board;
+
+    public ChessBoardInformation(Board board) {
+        this.board = board;
+    }
+
+    @Override
+    public boolean isEmpty(int row, int col) {
+        return this.board.getCell(row, col).getPiece() == null
+    }
+
+    @Override
+    public boolean isEmpty(Position position) {
+        return this.isEmpty(position.getRow(), position.getCol());
+    }
+
+    @Override
+    public Identifier getTeamIdentifier(Position position) {
+        int row = position.getRow();
+        int col = position.getCol();
+        return this.board.getCell(row, col).getPiece().getTeamIdentifier();
+    }
+
+    @Override
+    public Identifier getTypeIdentifier(Position position) {
+        int row = position.getRow();
+        int col = position.getCol();
+        return this.board.getCell(row,col).getPiece().getTypeIdentifier();
+    }
+
+    @Override
+    public int getBoardSize() {
+        return this.board.getRows();
+    }
+}
