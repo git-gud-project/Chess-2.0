d8981ad232b51ef4e573aa709516e4e5f00a90eb
Oscar <oscarmar@student.chalmers.se>
2022-02-22 16:46:28 +0100

    Added comments to the team interface.

--- src/main/java/com/chess/model/Team.java
+++ src/main/java/com/chess/model/Team.java
@@ -1,12 +1,16 @@
 package com.chess.model;
-import java.awt.*;
-import java.util.HashMap;
-
-import com.chess.model.chess.PieceType;
-import com.chess.utils.Event;
 
 public interface Team {
 
+    /**
+     * Gets the identifying name for the team.
+     * @return The identifying name for the team.
+     */
     public String getName();
+
+    /**
+     * Gets the remaining time a team has to play.
+     * @return The remaining time a team has to play.
+     */
     public Time getTime();
 }
